066a01bb1e6fc791829c80c7d5521df98477ffc2
Ignacio Borrajo <ignacio.borrajo@edisa.com>
2026-06-03 20:09:17 +0200

    feat(announcements): vistas modal/seen + plantilla _winner_modal.html
    GET /anuncios/<id>/ devuelve el HTML del modal de ganador (con título
    dinámico, lista de ganadores y emoji de trofeo). POST /anuncios/<id>/seen
    crea WinnerAnnouncementSeen y devuelve la URL del siguiente anuncio
    pendiente vía X-Modal-Next para encadenar.

--- announcements/views.py
+++ announcements/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from django.views import View
 
-# Create your views here.
+from .models import WinnerAnnouncement, WinnerAnnouncementSeen
+
+
+class AnnouncementModalView(LoginRequiredMixin, View):
+    def get(self, request, pk):
+        ann = get_object_or_404(
+            WinnerAnnouncement.objects.prefetch_related("winners").select_related("scope_round"),
+            pk=pk,
+        )
+        return render(
+            request,
+            "announcements/_winner_modal.html",
+            {"announcement": ann},
+        )
+
+
+class AnnouncementSeenView(LoginRequiredMixin, View):
+    def post(self, request, pk):
+        ann = get_object_or_404(WinnerAnnouncement, pk=pk)
+        WinnerAnnouncementSeen.objects.get_or_create(announcement=ann, user=request.user)
+
+        next_ann = (
+            WinnerAnnouncement.objects
+            .exclude(seen_by__user=request.user)
+            .exclude(pk=ann.pk)
+            .order_by("created_at")
+            .first()
+        )
+        resp = HttpResponse(status=204)
+        if next_ann is not None:
+            resp["X-Modal-Next"] = reverse("announcements:modal", args=[next_ann.id])
+        return resp
