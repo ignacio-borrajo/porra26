9febcb2e26f3f3a003f791362b6465ba69c689ef
Ignacio Borrajo <ignacio.borrajo@edisa.com>
2026-07-16 10:10:50 +0200

    style(sorteo): elimina import sin usar tras el cambio de mixin

--- raffle/views.py
+++ raffle/views.py
@@ -1,5 +1,4 @@
 from django.contrib import messages
-from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import JsonResponse
 from django.shortcuts import redirect, render
 from django.urls import reverse
